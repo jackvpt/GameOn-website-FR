8f2b974cc479c16db388f7ecb464fe70c5f5a530
Jacques Verpoest <jacques.verpoest@gmail.com>
2025-01-03 22:33:30 +0100

    reorganization & comments

--- starterOnly/assets/js/modal.js
+++ starterOnly/assets/js/modal.js
@@ -1,12 +1,11 @@
-
-
-// DOM Elements
+/**
+ * GET DOM ELEMENTS
+ */
 const modalbg = document.querySelector(".bground")
 const modalBtn = document.querySelectorAll(".modal-btn")
 const formData = document.querySelectorAll(".formData")
 const modalBody = document.querySelector(".modal-body")
 const modalBodySubmit = document.querySelector(".modal-body-submit")
-
 const first = document.getElementById("first")
 const errorFirst = document.getElementById("error-first")
 const last = document.getElementById("last")
@@ -17,14 +16,27 @@ const birthdate = document.getElementById("birthdate")
 const errorBirthdate = document.getElementById("error-birthdate")
 const quantity = document.getElementById("quantity")
 const errorQuantity = document.getElementById("error-quantity")
-
 const errorLocation = document.getElementById("error-location")
 const checkbox1 = document.getElementById("checkbox1")
 const errorCheckbox1 = document.getElementById("error-checkbox1")
-
 const closeBtn = document.getElementById("btn-close")
 
-// Edit Navbar
+setEventListeners()
+
+/**
+ * SET ALL EVENT LISTENERS
+ */
+function setEventListeners() {
+  // Signup button
+  modalBtn.forEach((btn) => btn.addEventListener("click", launchModal))
+  // Modal close button
+  closeBtn.addEventListener("click", closeModal)
+}
+
+/**
+ * EDIT NAVBAR
+ * When clicking on navbar burger icon, the navbar is responsive
+ */
 function editNav() {
   var topNav = document.getElementById("myTopnav")
   if (topNav.className === "topnav") {
@@ -34,23 +46,26 @@ function editNav() {
   }
 }
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal))
-
-closeBtn.addEventListener("click", closeModal)
-
-// launch modal form
+/**
+ * LAUNCH MODAL FORM
+ */
 function launchModal() {
   modalbg.style.display = "flex"
   modalBody.style.display = "flex"
   modalBodySubmit.style.display = "none"
 }
 
-// Close modal event
+/**
+ * CLOSE MODAL FORM
+ */
 function closeModal() {
   modalbg.style.display = "none"
 }
 
+/**
+ * CHECK FIRST NAME
+ * @returns true if item is valid or false
+ */
 function checkFirstName() {
   if (!first.checkValidity()) {
     errorFirst.textContent =
@@ -63,18 +78,26 @@ function checkFirstName() {
   return true
 }
 
+/**
+ * CHECK LAST NAME
+ * @returns true if item is valid or false
+ */
 function checkLastName() {
   if (!last.checkValidity()) {
     errorLast.textContent =
       "Veuillez entrer 2 caractères ou plus pour le champ du prénom"
-      first.classList.add("input-invalid")
-      return false
+    first.classList.add("input-invalid")
+    return false
   }
   errorLast.textContent = ""
   first.classList.remove("input-invalid")
   return true
 }
 
+/**
+ * CHECK EMAIL
+ * @returns true if item is valid or false
+ */
 function checkEmail() {
   if (!email.checkValidity()) {
     errorEmail.textContent = "Veuillez entrer une adresse e-mail valide."
@@ -87,6 +110,10 @@ function checkEmail() {
   return true
 }
 
+/**
+ * CHECK BIRTH DATE
+ * @returns true if item is valid or false
+ */
 function checkBirthdate() {
   if (!birthdate.checkValidity()) {
     errorBirthdate.textContent = "Vous devez entrer votre date de naissance."
@@ -98,8 +125,12 @@ function checkBirthdate() {
   return true
 }
 
+/**
+ * CHECK QUANTITY
+ * @returns true if item is valid or false
+ */
 function checkQuantity() {
-  if (!quantity.checkValidity() ) {
+  if (!quantity.checkValidity()) {
     errorQuantity.textContent = "Veuillez entrer un nombre entre 0 et 99."
     first.classList.add("input-invalid")
     return false
@@ -109,6 +140,10 @@ function checkQuantity() {
   return true
 }
 
+/**
+ * CHECK LOCATION
+ * @returns true if one radio button is checked or false
+ */
 function checkLocation() {
   const selectedLocation = document.querySelector(
     'input[name="location"]:checked'
@@ -121,6 +156,10 @@ function checkLocation() {
   return true
 }
 
+/**
+ * CHECK GENERAL CONDITIONS
+ * @returns true if checkbox is checked or false
+ */
 function checkConditions() {
   if (!checkbox1.checked) {
     errorCheckbox1.textContent = "Vous devez accepter les termes et conditions."
@@ -130,6 +169,11 @@ function checkConditions() {
   return true
 }
 
+/**
+ * VALIDATE THE WHOLE FORM
+ * @param {*} event
+ * @returns true if all items are valid or false
+ */
 function validate(event) {
   event.preventDefault()
 
@@ -144,9 +188,9 @@ function validate(event) {
   if (!checkConditions()) isValid = false
 
   if (isValid) {
-    document.forms["reserve"].reset()
-    modalBody.style.display = "none"
-    modalBodySubmit.style.display = "flex"
+    document.forms["reserve"].reset() // Reset all fields
+    modalBody.style.display = "none" // Hide the submit form
+    modalBodySubmit.style.display = "flex" // Show the success message
   }
 
   return isValid
